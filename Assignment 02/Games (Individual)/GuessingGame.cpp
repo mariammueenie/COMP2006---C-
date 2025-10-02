9451440812792ffb9566f18ac61b9b3cf5f48276
mariammueenie <156461593+mariammueenie@users.noreply.github.com>
2025-10-02 18:38:53 -0400

    Create GuessingGame.cpp
    Made separate file for each game's logic, to make it easier/faster to debug.

---
+++ Assignment 02/Games (Individual)/GuessingGame.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <ctime>
+#include <limits>
+#include <iomanip>
+#include <cctype>
+using namespace std; 
+
+/*
+// PROGRAM OUTLINE
+PART A: INTRODUCTION, TITLE, GET NAME FROM USER, GAME CHOICE SCREEN LOOP (Fayzabanu)
+PART B: GAME 1: Guessing Game (Mariam)
+Part C: GAME 2: Roll the dice (Fayzabanu)
+Part D: GAME 3: Lottery (Mariam)
+*/
+
+int main() {
+
+string firstName, lastName;
+int gameChoice;
+
+// Ask for players first and last name
+    cout << "Welcome to The Mini Arcade!!\n" << endl;
+    cout << "Please enter your first name:\n ";
+    cin >> firstName;
+    cout << "Please enter your last name:\n ";
+    cin >> lastName;
+
+    // Greet the player
+    cout << "Hello," << firstName << " " << lastName << "! Lets play a game.\n" << endl;
+
+
+// Guessing game logic here
+                cout << "Hello, " << firstName << " welcome to the Guessing Game!" << endl;
+                
+                // Explain game to user.
+                cout << "In this game, you will try to guess a number between 1 and 100." << endl;
+                cout << "You will have 3 attempts to guess the correct number." << endl;
+
+                char playAgain;
+
+                do {
+                // Limit the number of attempts to 3
+                int limit = 3; 
+                int count = 0;
+                int randomNumber = (rand() % 100) + 1; // Random number between 1 and 100
+
+                do {
+                    // std::cout << "Count." << count << std::endl;
+                    // Generate random number between 1 and 100
+
+                    // Get user's guess with validation loop
+                    cout << "Enter your guess (1-100): ";
+                    int userGuess;
+                    cin >> userGuess;
+                    
+                    if (!cin) {
+                        cout << "Invalid input. Please enter a number between 1 and 100." << endl;
+                        cin.clear(); // Clear the error flag
+                        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Discard invalid input
+                        continue; // Skip to the next iteration of the loop
+                    }
+
+                    if (userGuess < 1 || userGuess > 100) {
+                        cout << "Invalid input. Please enter a number between 1 and 100." << endl;
+                        continue; // Skip to the next iteration of the loop
+
+                    }
+
+                    count++; // Increment attempt count
+                    cout << "Attempt " << count << " of " << limit << "." << endl;
+                    cout << "Try again." << endl;
+
+                    if (userGuess < randomNumber) {
+                        cout << "Too low!" << endl;
+                    }
+
+                    else if (userGuess > randomNumber) {
+                        cout << "Too high!" << endl;
+                    }
+
+                    else if (userGuess == randomNumber) {
+                        cout << "Congratulations " << firstName << "! You guessed the correct number!" << endl;
+                        break; // Exit the loop if the user guesses correctly
+                    }
+                    if (count == limit) {
+                        cout << "Sorry, you've used all your attempts. The correct number was " << randomNumber << "." << endl;
+                    }
+                } while (count < limit);
+                
+                // Ask if the user wants to play again
+                    cout << "Do you want to play again? (y/n): ";
+                    cin >> playAgain;
+
+            } while  (tolower(playAgain) == 'y');
+
+                cout << "Returning to main menu..." << endl;
+            
+            } // End of case 1
+        
